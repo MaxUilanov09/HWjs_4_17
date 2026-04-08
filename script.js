0e5b448104bdfda9a99151b4e959b034311c0579
MaxUilanov09 <max.ulyanov7@gmail.com>
2026-04-08 19:16:16 +0100

    test

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function getStudents() {
-
+    const data = fetch('https://raw.githubusercontent.com/MaxUilanov09/HWjs_4_17/main/students.json').then(res => res.json())
+    return data;
 }
+
+console.log('a', getStudents());
 
 function renderStudents(students) {
 
