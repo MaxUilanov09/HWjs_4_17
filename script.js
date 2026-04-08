73543643e4db322b96fde0c29a97e341bec2db20
MaxUilanov09 <max.ulyanov7@gmail.com>
2026-04-08 19:36:39 +0100

    test4

--- script.js
+++ script.js
@@ -5,10 +5,24 @@ function getStudents() {
     return data;
 }
 
-console.log('a', getStudents().then(x => x));
+console.log('a', getStudents().then(x => console.log('b', x)));
 
 function renderStudents(students) {
+    students.forEach(Obj => {
+        let markup = `
+        <tr>
+            <th>${Obj.id}</th>
+            <th>${Obj.name}</th>
+            <th>${Obj.age}</th>
+            <th>${Obj.course}</th>
+            <th>${Obj.skills}</th>
+            <th>${Obj.email}</th>
+            <th>${Obj.isEnrolled}</th>
+            <th>${Obj}</th>
+        </tr>
+        `;
 
+    })
 }
 
 function addStudent(e) {
