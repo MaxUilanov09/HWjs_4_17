9b14424b328f5fa32a2a70a34782cebec941ce29
MaxUilanov09 <max.ulyanov7@gmail.com>
2026-04-08 19:48:11 +0100

    add test

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const student_body = document.querySelector('#students-table-body');
+const form = document.querySelector('#add-student-form');
 
 function getStudents() {
-    const data = fetch('https://maxuilanov09.github.io/HWjs_4_17/students.json')
+    const dataS = fetch('https://maxuilanov09.github.io/HWjs_4_17/students.json')
         .then(res => res.json())
-        .then(data => renderStudents(data.students));
-    console.log('c', data);
-    return data;
+        .then(data => renderStudents(data.students))
+        .then(data => {return data.students});
+    console.log('c', dataS);
+    return dataS;
 }
 
 function renderStudents(students) {
@@ -26,8 +28,23 @@ function renderStudents(students) {
     })
 }
 
-function addStudent(e) {
-
+function addStudent() {
+    let dataObj = {}
+    Array(...form.children).forEach(elem => {
+        console.log([elem, elem.children.length]);
+        if (elem.children.length !== 0) {
+            dataObj[elem.id] = elem.firstChild.value
+        }
+    });
+    console.log('dataObj', dataObj);
+    fetch('https://maxuilanov09.github.io/HWjs_4_17/students.json', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataObj)
+    });
+    getStudents();
 }
 
 function updateStudent(id) {
